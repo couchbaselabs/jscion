5575268ee3c065b11b4bbf247df730d8c9b7a8da
Steve Yen <steve.yen@gmail.com>
2013-09-08 23:18:18 -0700

    Added collectProperties().

--- static/js/jscion.js
+++ static/js/jscion.js
@@ -1,8 +1,33 @@
 function makeCtx(data) {
   function get(ident) { return { err: null, result: data[ident] }; }
-  function getClass(dobj) { return get("class-" + dobj["class"]); }
+  function getClass(dobj) { return getClassByName(dobj["class"]); }
+  function getClassByName(name) { return get("class-" + name); }
   return { "get": get,
-           "getClass": getClass };
+           "getClass": getClass,
+           "getClassByName": getClassByName };
+}
+
+// Walk the superclasses, gathering properties into the out dict, with
+// key of propertyName, val of array of properties, most specific
+// subclassed property coming first.  For example:
+// out == { "weight": [trollProperty, monsterProperty, actorProperty] };
+function collectProperties(ctx, cls, out) {
+  if (!cls) {
+    return null;
+  }
+  var s = ctx.getClassByName(cls.super);
+  if (s.err) {
+    return s.err;
+  }
+  var err = collectProperties(ctx, s.result, out);
+  if (err) {
+    return err;
+  }
+  _.each(cls.properties, function(property) {
+      var arr = out[property.name] || [];
+      arr.unshift(property);
+      out[property.name] = arr;
+    });
 }
 
 function render(ctx, ident) {
@@ -14,5 +39,10 @@ function render(ctx, ident) {
   if (c.err || !c.result) {
     return { err: c.err || ("no class for obj with ident: " + ident) };
   }
-  return { result: JSON.stringify(c.result) + " " + JSON.stringify(o.result) };
+  var properties = {};
+  var err = collectProperties(ctx, c.result, properties);
+  if (err) {
+    return { err: err };
+  }
+  return { result: JSON.stringify(properties) + " " + JSON.stringify(o.result) };
 }
