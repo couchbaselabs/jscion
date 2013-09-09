53ff29af101c230497fbfa137297cb3f30fe2107
Steve Yen <steve.yen@gmail.com>
2013-09-08 22:44:01 -0700

    Added makeCtx() and render() uses ctx.getClass().

--- static/js/jscion.js
+++ static/js/jscion.js
@@ -1,3 +1,18 @@
-function render(ctx, ident) {
+function makeCtx(data) {
+  function get(ident) { return { err: null, result: data[ident] }; }
+  function getClass(dobj) { return get("class-" + dobj["class"]); }
+  return { "get": get,
+           "getClass": getClass };
 }
 
+function render(ctx, ident) {
+  var o = ctx.get(ident);
+  if (o.err || !o.result) {
+    return { err: o.err || ("no object with ident: " + ident) };
+  }
+  var c = ctx.getClass(o.result);
+  if (c.err || !c.result) {
+    return { err: c.err || ("no class for obj with ident: " + ident) };
+  }
+  return { result: JSON.stringify(c.result) + " " + JSON.stringify(o.result) };
+}
