ffa450f82f2d40af4328722b19b506f015b1b439
Steve Yen <steve.yen@gmail.com>
2013-09-09 01:25:35 -0700

    Turned makeCtx() into jscion().

--- static/js/jscion.js
+++ static/js/jscion.js
@@ -1,73 +1,83 @@
-function makeCtx(data) {
+function jsion(data) {
+  var ctx = { "get": get,
+              "getClass": getClass,
+              "getClassByName": getClassByName,
+              "render": render,
+              "renderObj": renderObj,
+              "renderObjWithClass": renderObjWithClass };
+  return ctx;
+
   function get(ident) { return { err: null, result: data[ident] }; }
   function getClass(obj, defaultClassName) {
     return getClassByName(obj["class"] || defaultClassName);
   }
   function getClassByName(name) { return get("class-" + name); }
-  return { "get": get,
-           "getClass": getClass,
-           "getClassByName": getClassByName };
-}
 
-// Gather meta collection info from superclasses into the out dict,
-// where subclass values override superclass values.  The metaName can
-// be "properties", "methods", "validations", etc.
-function collectMeta(ctx, cls, metaName, out) {
-  if (!cls) {
-    return null;
+  function render(ident) {
+    var o = get(ident);
+    if (o.err || !o.result) {
+      return { err: o.err || ("no object with ident: " + ident) };
+    }
+    return renderObj(o.result);
   }
-  _.each(cls[metaName], function(x) {
-      out[x.name] = _.defaults(out[x.name] || {}, x)
-    });
-  var s = ctx.getClassByName(cls.super);
-  if (s.err) {
-    return s.err;
-  }
-  return collectMeta(ctx, s.result, metaName, out); // Recurse on super.
-}
 
-function render(ctx, ident) {
-  var o = ctx.get(ident);
-  if (o.err || !o.result) {
-    return { err: o.err || ("no object with ident: " + ident) };
+  function renderObj(obj) {
+    var c = getClass(obj);
+    if (c.err || !c.result) {
+      return { err: c.err || ("no class for obj: " + JSON.stringify(obj)) };
+    }
+    return renderObjWithClass(obj, c.result);
   }
-  return renderObj(ctx, o.result);
-}
 
-function renderObj(ctx, obj) {
-  var c = ctx.getClass(obj);
-  if (c.err || !c.result) {
-    return { err: c.err || ("no class for obj: " + JSON.stringify(obj)) };
-  }
-  return renderObjWithClass(ctx, obj, c.result);
-}
-
-function renderObjWithClass(ctx, obj, cls) {
-  var properties = {};
-  var err = collectMeta(ctx, cls, "properties", properties);
-  if (err) {
-    return { err: err };
-  }
-  var s = _.map(properties, function(p, k) {
-      var v = obj[k];
-      var pc = p.class || "property";
-      var pt = p.propertyType || "string";
-      var ptc = ctx.getClassByName(pt);
-      if (!ptc.err && ptc.result) {
-        if (pc == "propertyArray") {
-          v = _.map(v, function(vx) {
-              var r = renderObjWithClass(ctx, vx, ptc.result);
-              return r.err || r.result;
-            }).join("<hr/>");
-        } else {
-          var r = renderObjWithClass(ctx, v, ptc.result);
-          v = r.err || r.result;
+  function renderObjWithClass(obj, cls) {
+    var f = flattenProperties(cls);
+    if (f.err || !f.result) {
+      return { err: f.err || ("no properties for cls: " + JSON.stringify(cls)) };
+    }
+    var s = _.map(f.result, function(p, k) {
+        var v = obj[k];
+        var pc = p.class || "property";
+        var pt = p.propertyType || "string";
+        var ptc = getClassByName(pt);
+        if (!ptc.err && ptc.result) {
+          if (pc == "propertyArray") {
+            v = _.map(v, function(vx) {
+                var r = renderObjWithClass(vx, ptc.result);
+                return r.err || r.result;
+              }).join("<hr/>");
+          } else {
+            var r = renderObjWithClass(v, ptc.result);
+            v = r.err || r.result;
+          }
         }
+        if (k == "class" && !v) {
+          v = cls.name;
+        }
+        return "<li>" + k + ":" + v + "</li>";
+      }).join("\n");
+    return { result: "<ul>" + s + "</ul>" };
+  }
+
+  // Flatten collections from a object hierarchy into the out dict,
+  // where child values override parent values.  For classes, the
+  // collName can be "properties", "methods", "validations", etc., and
+  // the parentName can be "super".
+  function flattenHierarchy(obj, upFuncName, parentName, collName, out) {
+    while (obj) {
+      _.each(obj[collName], function(x) {
+        out[x.name] = _.defaults(out[x.name] || {}, x)
+      });
+      var p = ctx[upFuncName](obj[parentName]);
+      if (p.err) {
+        return p.err;
       }
-      if (k == "class" && !v) {
-        v = cls.name;
-      }
-      return "<li>" + k + ":" + v + "</li>";
-    }).join("\n");
-  return { result: "<ul>" + s + "</ul>" };
+      obj = p.result;
+    }
+  }
+
+  function flattenProperties(cls) {
+    var out = {}
+    var err = flattenHierarchy(cls, "getClassByName", "super", "properties", out);
+    return { err: err, result: out };
+  }
 }
