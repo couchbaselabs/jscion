1202ff75d7066c3245dffa11fa65e2148de06031
Steve Yen <steve.yen@gmail.com>
2013-09-16 18:44:56 -0700

    Split content() from contentJSON().

--- server.go
+++ server.go
@@ -28,25 +28,42 @@ func main() {
 		fmt.Printf("  -%s=%s\n", f.Name, f.Value)
 	})
 
-	dataHandler := func(w http.ResponseWriter, r *http.Request) {
-		d, err := json.Marshal(content(*dataPath, *dataSuffix, map[string]interface{}{}))
+	http.HandleFunc("/data.json", func(w http.ResponseWriter, r *http.Request) {
+		m, err := contentJSON(*dataPath, *dataSuffix, map[string]interface{}{})
+		if err != nil {
+			log.Printf("error: collecting content: %s, err: %v\n", *dataPath, err)
+			return
+		}
+		d, err := json.Marshal(m)
 		if err != nil {
 			log.Printf("error: marshaling content: %s, err: %v\n", *dataPath, err)
 			return
 		}
 		w.Write(d)
-	}
-
-	http.HandleFunc("/data.json", dataHandler)
+	})
 
 	http.Handle("/", http.FileServer(http.Dir(*staticPath)))
 
 	http.ListenAndServe(*addr, nil)
 }
 
-// Reads all the json files in a directory tree into a map.
-func content(root, suffix string, res map[string]interface{}) map[string]interface{} {
-	filepath.Walk(root, func(path string, f os.FileInfo, err error) error {
+func contentJSON(root, suffix string, res map[string]interface{}) (map[string]interface{}, error) {
+	return res, content(root, suffix, func(path, name string, b []byte) error {
+		key := name[0:len(name)-len(suffix)]
+		var val interface{}
+		err := json.Unmarshal(b, &val)
+		if err != nil {
+			log.Printf("error: parsing JSON file: %s, err: %v\n", path, err)
+			return err
+		}
+		res[key] = val
+		return nil
+	})
+}
+
+// Reads files in a directory tree into a map.
+func content(root, suffix string, visitor func(path, name string, b []byte) error) error {
+	return filepath.Walk(root, func(path string, f os.FileInfo, err error) error {
 		if f.IsDir() || !strings.HasSuffix(path, suffix) {
 			return nil
 		}
@@ -54,15 +71,6 @@ func content(root, suffix string, res map[string]interface{}) map[string]interfa
 		if err != nil {
 			return err
 		}
-		key := f.Name()[0:len(f.Name())-len(suffix)]
-		var val interface{}
-		err = json.Unmarshal(b, &val)
-		if err != nil {
-			log.Printf("error: parsing file: %s, err: %v\n", path, err)
-			return err
-		}
-		res[key] = val
-		return nil
+		return visitor(path, f.Name(), b)
 	})
-	return res
 }
