db0898753394f89e1c5fa5304fd528810f75b4fc
Steve Yen <steve.yen@gmail.com>
2013-09-19 09:35:08 -0700

    Renamed data dir to apps.

--- server.go
+++ server.go
@@ -16,10 +16,8 @@ import (
 
 var addr = flag.String("addr", ":8080",
 	"HTTP/REST listen address")
-var dataPath = flag.String("dataPath", "./data",
-	"data directory")
-var dataSuffix = flag.String("dataSuffix", ".json",
-	"suffix for data files")
+var appsPath = flag.String("appsPath", "./apps",
+	"apps directory")
 var staticPath = flag.String("staticPath", "./static",
 	"path to static web UI content")
 
@@ -28,46 +26,46 @@ func main() {
 	log.Printf("%s\n", os.Args[0])
 	flag.VisitAll(func(f *flag.Flag) { log.Printf("  -%s=%s\n", f.Name, f.Value) })
 
-	log.Printf("serving: /app/{app}/")
-	start(*addr, *dataPath, *dataSuffix, *staticPath)
+	log.Printf("serving: /apps/{app}/")
+	start(*addr, *appsPath, *staticPath)
 }
 
-func start(addr, dataPath, dataSuffix, staticPath string) {
+func start(addr, appsPath, staticPath string) {
 	r := mux.NewRouter()
-	sr := r.PathPrefix("/app/{app}/").Subrouter()
+	sr := r.PathPrefix("/apps/{app}/").Subrouter()
 	sr.HandleFunc("/data.json",
 		withApp(func(w http.ResponseWriter, r *http.Request, app string) {
-		m := map[string]interface{}{}
-		err := content(dataPath, dataSuffix, func(path, name string, b []byte) error {
-			key := name[0 : len(name)-len(dataSuffix)]
-			var val interface{}
-			err := json.Unmarshal(b, &val)
+			m := map[string]interface{}{}
+			err := content(appsPath, ".json", func(path, name string, b []byte) error {
+				key := name[0 : len(name)-len(".json")]
+				var val interface{}
+				err := json.Unmarshal(b, &val)
+				if err != nil {
+					log.Printf("error: parsing JSON file: %s, err: %v\n", path, err)
+					return err
+				}
+				m[key] = val
+				return nil
+			})
 			if err != nil {
-				log.Printf("error: parsing JSON file: %s, err: %v\n", path, err)
-				return err
+				log.Printf("error: collecting json: %s, err: %v\n", appsPath, err)
+				return
 			}
-			m[key] = val
-			return nil
-		})
-		if err != nil {
-			log.Printf("error: collecting json: %s, err: %v\n", dataPath, err)
-			return
-		}
-		d, err := json.Marshal(m)
-		if err != nil {
-			log.Printf("error: marshaling json: %s, err: %v\n", dataPath, err)
-			return
-		}
-		w.Write(d)
+			d, err := json.Marshal(m)
+			if err != nil {
+				log.Printf("error: marshaling json: %s, err: %v\n", appsPath, err)
+				return
+			}
+			w.Write(d)
 		}))
 	sr.HandleFunc("/data.js",
-		suffixHandler(dataPath, ".js", "/* %s.js */", "/* %s.js */"))
+		suffixHandler(appsPath, ".js", "/* %s.js */", "/* %s.js */"))
 	sr.HandleFunc("/data.css",
-		suffixHandler(dataPath, ".css", "/* %s.css */", "/* %s.css */"))
+		suffixHandler(appsPath, ".css", "/* %s.css */", "/* %s.css */"))
 	sr.HandleFunc("/data.ract",
-		suffixHandler(dataPath, ".ract", "<!-- {{>%s}} -->", "<!-- {{/%s}} -->"))
-    sr.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, staticPath + "/index.html")
+		suffixHandler(appsPath, ".ract", "<!-- {{>%s}} -->", "<!-- {{/%s}} -->"))
+	sr.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, staticPath+"/index.html")
 	})
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir(staticPath)))
 
@@ -88,13 +86,13 @@ func content(root, suffix string, visitor func(path, name string, b []byte) erro
 	})
 }
 
-func suffixHandler(root, suffix, beg, end string) func(w http.ResponseWriter, r *http.Request) {
+func suffixHandler(root, suffix, beg, end string) func(http.ResponseWriter, *http.Request) {
 	return withApp(func(w http.ResponseWriter, r *http.Request, app string) {
 		content(root, suffix, func(path, name string, b []byte) error {
 			base := name[0 : len(name)-len(suffix)]
-			w.Write([]byte(fmt.Sprintf(beg + "\n", base)))
+			w.Write([]byte(fmt.Sprintf(beg+"\n", base)))
 			w.Write(b)
-			w.Write([]byte(fmt.Sprintf(end + "\n", base)))
+			w.Write([]byte(fmt.Sprintf(end+"\n", base)))
 			w.Write([]byte("\n"))
 			return nil
 		})
@@ -104,7 +102,7 @@ func suffixHandler(root, suffix, beg, end string) func(w http.ResponseWriter, r 
 func withApp(orig func(http.ResponseWriter, *http.Request,
 	string)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-        // TODO: Auth checks.
+		// TODO: Auth checks.
 		orig(w, r, mux.Vars(r)["app"])
 	}
 }
