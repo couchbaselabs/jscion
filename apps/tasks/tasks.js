a7007ac8c6b0052626cf14b7e05c797e94cb2376
Steve Yen <steve.yen@gmail.com>
2013-09-23 08:42:53 -0700

    Using separate edit_obj for cancellability.

--- apps/tasks/tasks.js
+++ apps/tasks/tasks.js
@@ -6,21 +6,24 @@ function main(ctx, session) {
         var task = ctx.newObj("task").result;
         task.title = (event.node.value || "").trim();
         task.createdAt = task.updatedAt = new Date().toJSON();
-        event.node.value = "";
         render.get("tasks").unshift(task);
-        render.set({ "obj": task,
-                     "edit": false,
-                     "comment": false });
-        setTimeout(function () { event.node.focus(); }, 0 );
+        renderTask(render, task);
+        event.node.value = "";
+        event.node.focus();
       },
       "showTask": function(event) {
-        var task = _.find(tasks,
-                          function(task) { return task.createdAt == event.node.id; });
-        render.set({ "obj": task,
-                     "edit": false,
-                     "comment": false });
+        renderTask(render,
+                   _.find(tasks,
+                          function(task) { return task.createdAt == event.node.id; }));
       },
       "toggleEdit": function() { render.set("edit", !render.get("edit")); },
       "toggleComment": function() { render.set("comment", !render.get("comment")); }
     });
+}
+
+function renderTask(render, task) {
+  render.set({ "obj": task,
+               "edit_obj": _.clone(task),
+               "edit": false,
+               "comment": false });
 }
