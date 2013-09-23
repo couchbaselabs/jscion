74562176e245ba9d906c21d48a8688b53da7a3c7
Steve Yen <steve.yen@gmail.com>
2013-09-22 23:51:10 -0700

    Initial edit and comment buttons.

--- apps/tasks/tasks.js
+++ apps/tasks/tasks.js
@@ -1,20 +1,26 @@
 function main(ctx, session) {
   var tasks = session.tasks = session.tasks || [];
-  var ractive = session.render("tasks");
-  ractive.on({
+  var render = session.render("tasks");
+  render.on({
       "newTask": function(event) {
         var task = ctx.newObj("task").result;
         task.title = (event.node.value || "").trim();
         task.createdAt = task.updatedAt = new Date().toJSON();
         event.node.value = "";
-        ractive.get("tasks").unshift(task);
-        ractive.set("obj", task);
+        render.get("tasks").unshift(task);
+        render.set({ "obj": task,
+                     "edit": false,
+                     "comment": false });
         setTimeout(function () { event.node.focus(); }, 0 );
       },
       "showTask": function(event) {
-        ractive.set("obj", _.find(tasks, function(task) {
-              return task.title == event.node.text;
-            }));
+        var task = _.find(tasks,
+                          function(task) { return task.createdAt == event.node.id; });
+        render.set({ "obj": task,
+                     "edit": false,
+                     "comment": false });
       },
+      "toggleEdit": function() { render.set("edit", !render.get("edit")); },
+      "toggleComment": function() { render.set("comment", !render.get("comment")); }
     });
 }
