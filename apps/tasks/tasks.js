cf8c1133ca39dca6d10803e4af65ef4f669ce42e
Steve Yen <steve.yen@gmail.com>
2013-09-24 18:41:30 -0700

    Using link hash instead of showTask() event.

--- apps/tasks/tasks.js
+++ apps/tasks/tasks.js
@@ -1,48 +1,55 @@
 function main(ctx, session) {
   session.statusChoices = session.statusChoices || findStatusChoices(ctx)
+  session.tasks = session.tasks || [];
+  session.obj = findTask(ctx, session.tasks, session.ident) || session.obj;
 
-  var tasks = session.tasks = session.tasks || [];
-  var render = session.render("tasks");
+  if (!session.r) {
+    session.r = session.render("tasks");
+  } else {
+    session.r.update("tasks");
+  }
+  renderTask(session.r, session.obj);
 
-  render.on({
+  session.r.on({
       "newTask": function(event) {
         var task = ctx.newObj("task").result;
         task.title = (event.node.value || "").trim();
-        task.createdAt = task.updatedAt = new Date().toJSON();
-        render.get("tasks").unshift(task);
-        renderTask(render, task);
-        event.node.value = "";
-        event.node.focus();
-      },
-      "showTask": function(event) {
-        renderTask(render, findTask(tasks, event.node.id));
+        if (task.title) {
+          task.createdAt = task.updatedAt = new Date().toJSON();
+          task.ident = "task-" + task.createdAt.replace(/[^0-9]/g, '') +
+            Math.round(Math.random() * 10000);
+          session.r.get("tasks").unshift(task);
+          renderTask(session.r, task);
+          event.node.value = "";
+          event.node.focus();
+        }
       },
       "saveTask": function() {
-        var edit = render.get("objEdit");
-        var orig = findTask(render.get("tasks"), edit.createdAt);
+        var edit = session.r.get("objEdit");
+        var orig = findTask(ctx, session.r.get("tasks"), edit.ident);
         _.extend(orig, edit);
         _.each(_.keys(orig), function(k) {
             if (_.isString(orig[k])) { orig[k] = orig[k].trim(); }
           });
-        renderTask(render, orig);
-        render.update("tasks");
+        renderTask(session.r, orig);
+        session.r.update("tasks");
       },
       "editTask": function() {
-        renderTask(render, render.get("obj"), { "doEdit": !render.get("doEdit") });
-        if (render.get("doEdit")) {
+        renderTask(session.r, session.r.get("obj"), { "doEdit": !session.r.get("doEdit") });
+        if (session.r.get("doEdit")) {
           setTimeout(function() { $("#objEdit_title").focus(); });
         }
       },
       "addComment": function() {
-        renderTask(render, render.get("obj"), { "doComment": !render.get("doComment") });
-        if (render.get("doComment")) {
+        renderTask(session.r, session.r.get("obj"), { "doComment": !session.r.get("doComment") });
+        if (session.r.get("doComment")) {
           setTimeout(function() { $("#commentMessage").focus(); });
         }
       },
       "saveComment": function() {
-        var task = render.get("obj");
+        var task = session.r.get("obj");
         if (task) {
-          var m = (render.get("commentMessage") + "").trim();
+          var m = (session.r.get("commentMessage") + "").trim();
           if (m) {
             var c = ctx.newObj("taskMessage").result;
             c.createdAt = c.updatedAt = new Date().toJSON();
@@ -51,13 +58,14 @@ function main(ctx, session) {
             task.messages.push(c);
           }
         }
-        renderTask(render, task);
+        renderTask(session.r, task);
       }
     });
 }
 
-function findTask(tasks, createdAt) {
-  return _.find(tasks, function(task) { return task.createdAt == createdAt; });
+function findTask(ctx, tasks, ident) {
+  return ctx.findObj(where).result || _.find(tasks, where);
+  function where(task) { return task.ident == ident; };
 }
 
 function renderTask(render, task, extras) {
